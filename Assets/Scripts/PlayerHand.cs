ad2e41d0ff1118c084702f3d022fe9d42ab51ed1
Brian Lee <blee1126@gmail.com>
2021-09-14 13:45:52 -0500

    update!

--- Assets/Scripts/PlayerHand.cs
+++ Assets/Scripts/PlayerHand.cs
@@ -9,35 +9,31 @@ public class PlayerHand : MonoBehaviour
     public GameObject[] gamePieces;
     public GameObject[] playerHand;
 
-    public GameObject[] pieceCoaster;
-
     public Text scoreText;
 
 
     void Start()
     {
-    //get game all game pieces and player piece objects
+        //get all game pieces and player piece objects
         gamePieces = GameObject.FindGameObjectsWithTag("Dragabble");
-        pieceCoaster = GameObject.FindGameObjectsWithTag("PieceCoaster");
         playerHand = GameObject.FindGameObjectsWithTag("PlayerHand");
 
-
-
-
-    //loop through each player piece object and position a random gamepice over it
-        for (var i = 0; i < pieceCoaster.Length; i++)
-        if(pieceCoaster[i].tag == "PieceCoaster")
+        // Bool check to see if a collider gameObject is at position
+        bool isObjectHere(Vector3 position)
         {
-                gamePieces[Random.Range(0, gamePieces.Length)].transform.position = pieceCoaster[i].transform.position;
-                
-                pieceCoaster[i].transform.tag = "PlayerHand";
-
-            //only transform a pleyerhand if there isn't a gamepiece there already
-
-            //how do we know if its there already?
-            
-        // Debug.Log(playerHand);
-            
+            Collider[] intersecting = Physics.OverlapSphere(position, 0.01f);
+            return (intersecting.Length != 0);
         }
+        //loop through each player piece object and position a random gamepiece over it
+        for (int i = 0; i < playerHand.Length; i++)
+        {
+            if(!isObjectHere(playerHand[i].transform.position))
+            {
+                var randomInt = Random.Range(0, gamePieces.Length);
+                gamePieces[randomInt].transform.position = playerHand[i].transform.position;
+            }
+        }
+
+
     }
 }
